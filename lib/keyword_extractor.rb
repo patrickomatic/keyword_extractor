d1effb78775ce300fce0b73906267dc17d775039
Patrick Carroll <patrick@patrickomatic.com>
2013-11-04 16:44:04 -0500

    Instantiate and use a Stopwords object

--- lib/keyword_extractor.rb
+++ lib/keyword_extractor.rb
@@ -1,21 +1,23 @@
 require 'keyword_extractor/keyword'
 require 'keyword_extractor/document'
 require 'keyword_extractor/tfidf'
+require 'keyword_extractor/stopwords'
 
 # TODO
-# * support for a list of stop words
 # * should be able to extract keywords from on a corpus level and also across all of the corpuses
 # * make the various term frequency strategies pluggable/configurable
 module KeywordExtractor
   VERSION = '0.0.1'
 
   def self.extract_keywords(text)
-    if text.is_a? String
+    if text.instance_of? String
       text = [text]
-    elsif !text.is? Array
+    elsif !text.instance_of? Array
       raise "The text argument should either be a String or an Array"
     end
 
-    TFIDF.analyze(text.map {|t| Document.new(t)})
+    stopwords = Stopwords.new('stopwords/english.txt')
+
+    TFIDF.analyze(text.map {|t| Document.new(t, stopwords)})
   end
 end
